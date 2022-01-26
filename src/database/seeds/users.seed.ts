3fa3a372d9526f2a5e12222f852ce43d90b8eb58
Rustem Zhantemirov <zhantemirov.rustem@gmail.com>
2022-01-26 17:20:49 +0600

    fix(): fix seeds

--- src/database/seeds/users.seed.ts
+++ src/database/seeds/users.seed.ts
@@ -1,48 +1,45 @@
+import { PostEntity } from 'src/post/post.entity';
 import { UserEntity } from 'src/user/user.entity';
 import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
 
-type UserSeed = {
-  username: string;
-  email: string;
-  password: string;
-};
-
-const defaultUsers: Record<string, UserSeed> = {
-  user: {
-    username: 'Jone',
-    email: 'jone@email.com',
-    password: '123',
-  },
-  user2: {
-    username: 'Max',
-    email: 'max@email.com',
-    password: '123',
-  },
-};
-
 export default class CreateUsers implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
-    const usersSeeder = factory(UserEntity)();
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(UserEntity)
+      .values([
+        { username: 'Max', email: 'max@email.com', password: '123' },
+        { username: 'Jone', email: 'jone@email.com', password: '123' },
+        { username: 'Bill', email: 'bill@email.com', password: '123' },
+      ])
+      .execute();
 
-    // async function createUsers(user: UserSeed): Promise<void> {
-    //   const userCreated = await usersSeeder.create({
-    //     username: user.username,
-    //     email: user.email,
-    //     password: user.password,
-    //   });
-    // }
-
-    const usersPromises = Object.entries(defaultUsers).map(
-      async ([countUser, userData]) => {
-        const userCreated = await usersSeeder.create({
-          username: userData.username,
-          email: userData.email,
-          password: userData.password,
-        });
-      },
-    );
-
-    await Promise.all(usersPromises);
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(PostEntity)
+      .values([
+        {
+          title: 'Post 1',
+          description: 'Description 1',
+          image: 'post1.png',
+          userId: 1,
+        },
+        {
+          title: 'Post 2',
+          description: 'Description 2',
+          image: 'post2.png',
+          userId: 2,
+        },
+        {
+          title: 'Post 3',
+          description: 'Description 3',
+          image: 'post3.png',
+          userId: 3,
+        },
+      ])
+      .execute();
   }
 }
